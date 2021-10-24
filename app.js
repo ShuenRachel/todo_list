1b7cfc8844dbc4784255f57540af56d537976f93
ShuenRachel <shuen0410@gmail.com>
2021-10-24 19:24:40 +0800

    feat: add mongoose

--- app.js
+++ app.js
@@ -2,6 +2,21 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const mongoose = require('mongoose')
+mongoose.connect('mongodb://localhost/todo-list')
+// 取得資料庫連線狀態
+const db = mongoose.connection
+
+// 連線異常
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+
+// 連線成功
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
+
 app.get('/', (req, res) => {
   res.send('This server is for todo-list')
 })
