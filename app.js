746c75124bc24b868bc3971115cd1dbab7f0a813
ShuenRachel <shuen0410@gmail.com>
2021-10-25 13:12:23 +0800

    feat: add handlebars

--- app.js
+++ app.js
@@ -7,6 +7,11 @@ mongoose.connect('mongodb://localhost/todo-list')
 // 取得資料庫連線狀態
 const db = mongoose.connection
 
+const exphbs = require('express-handlebars')
+
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', 'hbs')
+
 // 連線異常
 db.on('error', () => {
   console.log('mongodb error!')
@@ -18,7 +23,7 @@ db.once('open', () => {
 })
 
 app.get('/', (req, res) => {
-  res.send('This server is for todo-list')
+  res.render('index')
 })
 
 app.listen(port, () => {
