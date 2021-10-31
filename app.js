de04898ae03dc1d93156bb71820eaa665bff5db8
ShuenRachel <shuen0410@gmail.com>
2021-10-31 11:21:04 +0800

    refactor: modify express routes

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const routes = require('./routes')
 
 const mongoose = require('mongoose')
 mongoose.connect('mongodb://localhost/todo-list')
 // 取得資料庫連線狀態
 const db = mongoose.connection
-const Todo = require('./models/todo')
 
 const exphbs = require('express-handlebars')
 const methodOverride = require('method-override')
@@ -25,68 +25,10 @@ app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
 app.use(express.urlencoded({ extended: true }))
+
 app.use(methodOverride('_method'))
 
-app.get('/', (req, res) => {
-  Todo.find()
-    .lean()
-    .sort({ _id: 'asc' })
-    .then(todos => res.render('index', { todos }))
-    .catch(error => console.log(error))
-})
-
-app.get('/todos/new', (req, res) => {
-  return res.render('new')
-})
-
-app.post('/todos', (req, res) => {
-  const name = req.body.name
-  
-  return Todo.create({ name })
-    .then(() => res.redirect('/'))
-    .catch(error => console.log(error))
-})
-
-app.get('/todos/:id', (req, res) => {
-  const id = req.params.id
-
-  return Todo.findById(id)
-    .lean()
-    .then(todo => res.render('details', { todo }))
-    .catch(error => console.log(error))
-})
-
-app.get('/todos/:id/edit', (req, res) => {
-  const id = req.params.id
-
-  return Todo.findById(id)
-    .lean()
-    .then(todo => res.render('edit', { todo }))
-    .catch(error => console.log(error))
-})
-
-app.put('/todos/:id', (req, res) => {
-  const id = req.params.id
-  const { name, isDone } = req.body
-
-  return Todo.findById(id)
-    .then(todo => {
-      todo.name = name
-      todo.isDone = isDone === 'on'
-      return todo.save()
-    })
-    .then(() => res.redirect(`/todos/${id}`))
-    .catch(error => console.log(error))
-})
-
-app.delete('/todos/:id', (req, res) => {
-  const id = req.params.id
-
-  return Todo.findById(id)
-    .then(todo => todo.remove())
-    .then(() => res.redirect(`/`))
-    .catch(error => console.log(error))
-})
+app.use(routes)
 
 app.listen(port, () => {
   console.log(`Todo-list is running on http://localhost:${port}`)
