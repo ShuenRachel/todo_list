ee8cdf9610ecc77e44589ca2b2688bd43ee10998
ShuenRachel <shuen0410@gmail.com>
2021-10-31 11:31:34 +0800

    refactor: modify mongoose config

--- app.js
+++ app.js
@@ -1,31 +1,17 @@
 const express = require('express')
-const app = express()
-const port = 3000
-const routes = require('./routes')
-
-const mongoose = require('mongoose')
-mongoose.connect('mongodb://localhost/todo-list')
-// 取得資料庫連線狀態
-const db = mongoose.connection
-
 const exphbs = require('express-handlebars')
 const methodOverride = require('method-override')
 
-// 連線異常
-db.on('error', () => {
-  console.log('mongodb error!')
-})
+const routes = require('./routes')
+require('./config/mongoose')
 
-// 連線成功
-db.once('open', () => {
-  console.log('mongodb connected!')
-})
+const app = express()
+const port = 3000
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
 app.use(express.urlencoded({ extended: true }))
-
 app.use(methodOverride('_method'))
 
 app.use(routes)
